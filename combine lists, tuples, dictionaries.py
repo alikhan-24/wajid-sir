6f1155c9da8d784b2cb27b2d486522ad8ed4614f
alikhan-24 <18muhammadalikhan@gmail.com>
2024-12-20 19:07:32 +0530

    Update combine lists, tuples, dictionaries.py

--- combine lists, tuples, dictionaries.py
+++ combine lists, tuples, dictionaries.py
@@ -1,8 +1,8 @@
-my_dict={"taha" : (90,100,95,),
-         "arsalan": (95,100,98),
-         "jibran" : (90,95,99)}
+my_dict={"ali" : (90,100,95,),
+         "taha": (95,100,98),
+         "kamran" : (90,95,99)}
 result=my_dict
 print(result["taha"])
-avg_score=sum(result["taha"]) / 3, sum(result["arsalan"])/3 ,sum(result["jibran"])/3
+avg_score=sum(result["taha"]) / 3, sum(result["ali"])/3 ,sum(result["kamran"])/3
 print(avg_score)
 print(max(avg_score))
